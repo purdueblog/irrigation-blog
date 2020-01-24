8a44a052f88a823e4f21d374268500768bff00b2
ok60subin <60subin@naver.com>
2020-01-25 01:17:06 +0900

    Hamburger!

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,9 +2,9 @@ import React from 'react'
 import styled, { css } from 'styled-components'
 import { Flex, Box } from 'grid-styled'
 import scrollToElement from 'scroll-to-element'
-import stylef, { keyframes } from 'styled-components';
-import 'styleOfNav.css'
+import './styleOfNav.css'
 import Name from './name'
+import logoCorn from './img/corn.png'
 
 import { media } from '../utils/style'
 
@@ -75,9 +75,59 @@ const Base = styled.div`
       display: none;
     }
   `}
-`
 
+
+`
 class NavBar extends React.Component {
+
+  movingdiv = null;
+  movinglist = null;
+  constructor(props){
+    super(props);
+    this.state = {btnClicked:1,num:0,pos:0} //btnClicked 가 1이면 없는거(시작:-250px)부터 시작, 0이면 있는거(시작:0px)부터 시작. 
+  }
+  toRight(){
+    let counter = setInterval(()=>{
+      console.log("R");
+      this.setState({num:this.state.num-1, pos:this.state.pos+5});
+      this.movingdiv.style.left=this.state.pos+"px";
+      if(this.state.num<=0) clearInterval(counter);
+    },25);
+  }
+  toLeft(){
+    
+    let counter = setInterval(()=>{
+      console.log("L");
+      this.setState({num:this.state.num+1,pos:this.state.pos-5});
+      this.movingdiv.style.left=this.state.pos+"px";
+      if(this.state.num>=50) clearInterval(counter);
+      },25);
+  }
+  makeNone(){
+    this.setState({ btnClicked: this.state.btnClicked + 1 })
+    console.log("false-true");
+    this.toLeft();
+    // this.movinglist.style.display='none';
+    // this.movingdiv.style.display='none';
+  }
+  makeTrue(){
+    this.setState({ btnClicked: this.state.btnClicked - 1 })
+    console.log("true-false");
+    this.toRight();
+    // this.movinglist.style.display='block';
+    // this.movingdiv.style.display='block';
+  }
+  
+  handleOnClick = () => {
+    console.log("clicked");
+    if(this.state.btnClicked==0){
+      this.makeNone();
+    }
+    else{
+      this.makeTrue();
+    }
+  }
+
   render() {
     const linkMap = this.props.children
       .map(el => {
@@ -86,8 +136,7 @@ class NavBar extends React.Component {
       })
       .filter(n => n != undefined)
       .reverse()
-
-    
+  
     const links = linkMap.map(function(link) {
       return (
         <li key={link.name}>
@@ -101,6 +150,7 @@ class NavBar extends React.Component {
         </li>
       )
     })
+
     return (
       <div>
         <Base {...this.props}>
@@ -108,24 +158,35 @@ class NavBar extends React.Component {
             <Box px={2} width={[1, 1 / 3, 2 / 6]}>
               <Name />
             </Box>
+            <img src={logoCorn}class="style4btn" onClick={this.handleOnClick}/>
             <Box px={2} width={[0, 2 / 3, 4 / 6]}>
               <ul>{links}</ul>
             </Box>
           </Flex>
         </Base>
-        <style>
-          @import url('https://fonts.googleapis.com/css?family=Indie+Flower&display=swap');
-        </style>
-        <div class="style4div">
-            <ul class="style4ul">
-              <li>
-                  Members
+        
+        <div class="style4div" ref={ref => {
+            this.movingdiv = ref;
+          }}>
+            <ul class="style4ul"ref={ref => {
+            this.movinglist = ref;
+          }}>
+              <li class="style4li">
+                  <a>Members</a>
               </li>
-              <li>
-                  About Project
+              <li class="style4li">
+                <details>
+                  <summary>Project</summary>
+                    <ul>
+                      <li class="style4li">Network</li>
+                      <li class="style4li">Hardware</li>
+                      <li class="style4li">Irrigation</li>
+                      <li class="style4li">Data</li>
+                    </ul>
+                </details>
               </li>
-              <li>
-                  Github Blog
+              <li class="style4li">
+                  <a href="">Github Blog</a>
               </li>
             </ul>
         </div>
